09067dc07b283803c43396576771519411343372
Breno Albuquerque <brenoralbu@hotmail.com>
2022-03-06 17:11:27 -0300

    estiliza header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import './Header.css';
 
 class Header extends React.Component {
   handleSum = () => {
+    const zero = 0;
     const { expenses } = this.props;
 
     if (expenses.length > 0) {
@@ -17,18 +18,37 @@ class Header extends React.Component {
 
       return totalSum.toFixed(2);
     }
-    return 0;
+    return zero.toFixed(2);
   }
 
   render() {
     const { userEmail } = this.props;
 
     return (
-      <>
-        <div data-testid="email-field">{ userEmail }</div>
-        <div data-testid="total-field">{ this.handleSum() }</div>
-        <div data-testid="header-currency-field">BRL</div>
-      </>
+      <header>
+
+        <div className="title-container">
+          <h1>TRYBE WALLET</h1>
+        </div>
+
+        <div className="infos-div" data-testid="email-field">
+          <span>Email:</span>
+          {' '}
+          { userEmail }
+        </div>
+        <div className="infos-div" data-testid="total-field">
+          <span>Despesa Total:</span>
+          {' '}
+          {'R$ '}
+          { this.handleSum() }
+        </div>
+        <div
+          className="infos-div"
+          data-testid="header-currency-field"
+        >
+          <span>BRL</span>
+        </div>
+      </header>
     );
   }
 }

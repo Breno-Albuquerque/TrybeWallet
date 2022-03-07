63b2c99982b682f1e09529730c018bcc0a141967
Breno Albuquerque <brenoralbu@hotmail.com>
2022-03-07 16:09:06 -0300

    requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './Table.css';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends React.Component {
+  getCoinName = (expense) => {
+    const { exchangeRates, currency } = expense;
+
+    const keyName = exchangeRates[currency].name;
+
+    const coinName = keyName.slice(0, keyName.indexOf('/'));
+
+    return coinName;
+  }
+
+  getExchangeValue = (expense) => {
+    const { exchangeRates, currency } = expense;
+
+    const { ask } = exchangeRates[currency];
+
+    return Number(ask).toFixed(2);
+  }
+
+  getConvertedValue = (expense) => {
+    const { exchangeRates, currency, value } = expense;
+
+    const { ask } = exchangeRates[currency];
+
+    return (Number(ask) * Number(value)).toFixed(2);
+  }
+
   render() {
+    const { expenses } = this.props;
+
     return (
       <div className="table-container">
         <table>
@@ -18,11 +47,28 @@ class Table extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ Number(expense.value).toFixed(2) }</td>
+              <td>{ this.getCoinName(expense) }</td>
+              <td>{ this.getExchangeValue(expense) }</td>
+              <td>{ this.getConvertedValue(expense) }</td>
+              <td>Real</td>
+              <td>Editar/excluir</td>
+            </tr>
+          )) }
         </table>
       </div>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,

c21bee61d77ed44f021c961d9319663c09f1fa6a
Breno Albuquerque <brenoralbu@hotmail.com>
2022-03-28 21:26:59 -0300

    Updates

---
+++ precache-manifest.43acdb47aa618dda68faef8c66e81c15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f06602d509ca42d0dbe869d9ec2711",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "7a3939774813e2d76d48",
+    "url": "/trybewallet/static/css/main.35e0b584.chunk.css"
+  },
+  {
+    "revision": "a2a16bbc78ca0100ac12",
+    "url": "/trybewallet/static/js/2.531b450b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.531b450b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3939774813e2d76d48",
+    "url": "/trybewallet/static/js/main.be1764d1.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
